25c8f2461ad95d74d64c34f5197d0f7f7f35096d
Lohran Mendes <lohranfm@gmail.com>
2025-03-24 03:50:24 -0300

    feat: solved question 3

--- script_03.js
+++ script_03.js
@@ -1,3 +1,4 @@
+import * as Utils from "./lib/utils.js";
 // Exercício 3 Funções de Ordenação - Parte 2
 
 // Crie uma aplicação que implemente uma biblioteca com 2 funções: sortStringAsc e sortStringDesc, recebendo um array como parâmetro. Crie um array com os itens: “Javascript”, “Kotlin”, “C”, “Rust”, “Python”, “Go” e “Basic” e use esse array para testar as duas funções, exibindo o resultado em 2 alerts separados e sequenciais.
@@ -7,3 +8,19 @@
 // Utilize alert.
 // Implemente os métodos de forma a não alterar o array original.
 // Use o operador de spread.
+const linguagens = [
+  "Javascript",
+  "Kotlin",
+  "C",
+  "Rust",
+  "Python",
+  "Go",
+  "Basic",
+];
+
+alert(`Array original:\n${linguagens}`);
+Utils.sortStringAsc(linguagens, "alert", "alert");
+Utils.sortStringDesc(linguagens, "alert", "alert");
+alert(
+  `Array original depois dos métodos usados para mostrar que não foi alterado:\n${linguagens}`
+);
