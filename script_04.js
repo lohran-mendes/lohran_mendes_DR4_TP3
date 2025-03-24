15dca4e06bf77d161b1685e887c1b6038d864c30
Lohran Mendes <lohranfm@gmail.com>
2025-03-24 17:11:58 -0300

    feat: solved questions 4 and 5

--- script_04.js
+++ script_04.js
@@ -5,3 +5,21 @@
 // Observações:
 
 // Utilize o objeto Intl.DisplayNames.
+
+// Peguei esse array na internet
+const countryCodes = [
+  "AF", "AL", "DZ", "AD", "AO", "AG", "AR", "AM", "AU", "AT", "AZ", "BS", "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BT", "BO", "BA", "BW", "BR", "BN", "BG", "BF", "BI", "CV", "KH", "CM", "CA", "CF", "TD", "CL", "CN", "CO", "KM", "CD", "CG", "CR", "HR", "CU", "CY", "CZ", "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "SZ", "ET", "FJ", "FI", "FR", "GA", "GM", "GE", "DE", "GH", "GR", "GD", "GT", "GN", "GW", "GY", "HT", "HN", "HU", "IS", "IN", "ID", "IR", "IQ", "IE", "IL", "IT", "JM", "JP", "JO", "KZ", "KE", "KI", "KP", "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT", "LU", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MR", "MU", "MX", "FM", "MD", "MC", "MN", "ME", "MA", "MZ", "MM", "NA", "NR", "NP", "NL", "NZ", "NI", "NE", "NG", "NO", "OM", "PK", "PW", "PA", "PG", "PY", "PE", "PH", "PL", "PT", "QA", "RO", "RU", "RW", "KN", "LC", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL", "SG", "SK", "SI", "SB", "SO", "ZA", "SS", "ES", "LK", "SD", "SR", "SE", "CH", "SY", "TJ", "TZ", "TH", "TL", "TG", "TO", "TT", "TN", "TR", "TM", "TV", "UG", "UA", "AE", "GB", "US", "UY", "UZ", "VU", "VA", "VE", "VN", "YE", "ZM", "ZW"
+];
+const h1 = document.querySelector('h1')
+const ul = document.createElement('ul')
+
+h1.insertAdjacentElement('afterend', ul)
+
+countryCodes.forEach((country)=>{
+  const regionName = new Intl.DisplayNames(["pt-br"], {
+    type: "region",
+  });
+const li = document.createElement('li')
+li.innerText = regionName.of(country)
+ul.appendChild(li)
+})
--- script_05.js
+++ script_05.js
@@ -1,3 +1,5 @@
+import {validateString} from './lib/utils.js'
+
 // Exercício 5 Nomes Próprios
 
 // Crie uma aplicação que contenha uma função que corrija as palavras de um nome próprio informado, colocando as primeiras letras em maiúsculas corretamente. Por exemplo, se o usuário informar “machado de assis” a aplicação exibe “Machado de Assis” corretamente.
@@ -6,3 +8,17 @@
 
 // Utilize prompt e alert.
 // Empregue uma lista simples de exceções como “de”, “do” ou “da”. Não é preciso esgotar todas as possibilidades.
+
+function formatName(){
+  let nome = prompt('Insira aqui o nome completo.')
+  if(validateString(nome, "alert")) {
+    const arrayName = nome.split(' ')
+    const formattedArray = arrayName.map((word)=> {
+      if(word.toLowerCase() === 'de' || word.toLowerCase() === 'do' || word.toLowerCase() === 'da') return word.toLowerCase()
+        const palavraTratada = word.toLowerCase().replace(word[0], word[0].toUpperCase())
+        return palavraTratada
+    })
+    alert(formattedArray.join(' '))
+  }
+}
+formatName()
