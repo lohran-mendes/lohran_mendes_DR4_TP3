652a27a3279563afc28d2942c406240f5563425d
Lohran Mendes <lohranfm@gmail.com>
2025-03-24 03:38:35 -0300

    feat: solved question 2

--- script_02.js
+++ script_02.js
@@ -1,3 +1,5 @@
+import * as Utils from "./lib/utils.js";
+
 // Exercício 2 Funções de Ordenação - Parte 1
 
 // Enunciado: Crie uma aplicação que implemente uma biblioteca com 2 funções: sortNumberAsc e sortNumberDesc, recebendo um array como parâmetro. Crie um array com 10 números e use esse array para testar as duas funções, exibindo o resultado em 2 alerts separados e sequenciais.
@@ -7,3 +9,12 @@
 // Utilize alert.
 // Implemente os métodos de forma a não alterar o array original.
 // Use o operador de spread.
+
+const arrayTeste = [1, 8, 4, 2, 3, 5, 7, 10, 9, 6];
+
+alert(`Array original: ${arrayTeste}`);
+Utils.sortNumberAsc(arrayTeste, "alert", "alert");
+Utils.sortNumberDesc(arrayTeste, "alert", "alert");
+alert(
+  `Array original depois dos métodos usados para mostrar que não foi alterado: ${arrayTeste}`
+);
