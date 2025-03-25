4627bf016e5b574dbec75d61d8ad129bb3fb179f
Lohran Mendes <lohranfm@gmail.com>
2025-03-25 13:40:10 -0300

    feat: solved questions 15 and 16

--- script_15.js
+++ script_15.js
@@ -7,3 +7,21 @@
 // Chame a função criada passando o número digitado pelo usuário.
 // O resultado deve ser exibido em uma tag <p> criado no seu HTML.
 // Observação: Utilize recursão.
+import { convertToNumber } from "./lib/utils.js";
+
+let numero = convertToNumber(
+  prompt("Qual o número que você deseja saber o fatorial?"),
+  "alert"
+);
+
+function fatorial(n) {
+  if (n === 0 || n === 1) return 1;
+  return n * fatorial(n - 1);
+}
+
+if (!isNaN(numero) && numero >= 0) {
+  let resultado = fatorial(numero);
+  const resposta = document.createElement("p");
+  resposta.innerText = `O fatorial de ${numero} é igual a: ${resultado}`;
+  document.body.appendChild(resposta);
+}
--- script_16.js
+++ script_16.js
@@ -7,3 +7,30 @@
 // Crie um novo array, contendo o valor da raiz quadrada de cada número do primeiro array em cada posição respectiva neste novo array.
 // Exiba este novo array.
 // Assim que terminar, salve todos os links para os exercícios realizados em PDF nomeando o arquivo conforme a regra “nome_sobrenome_DR4_TP3.PDF” e poste como resposta a este TP.
+import { convertToNumber } from "./lib/utils.js";
+
+function mapeiaArray() {
+  const array = [];
+  for (let i = 0; i < 5; i++) {
+    let valor;
+    valor = convertToNumber(prompt(`Insira o ${i + 1}º valor:`), "alert");
+    if (isNaN(valor)) {
+      return alert(
+        "todos os 5 valores devem ser valores númericos, tente novamente!"
+      );
+    }
+    array.push(valor);
+  }
+  const novoArray = array.map((valor) => {
+    if (Number.isInteger(Math.sqrt(valor))) return Math.sqrt(valor);
+    return Math.sqrt(valor).toFixed(2);
+  });
+  alert(
+    `Array informado:\n${array.join(
+      ",  "
+    )}\n\rArray novo com o valor de sua raiz quadrada:\n${novoArray.join(
+      ",  "
+    )}`
+  );
+}
+mapeiaArray();
